1bec9906e0dc7d63ab1f71e31ffa0ad102fd2c97
gierrejunior <gierremartins@gmail.com>
2024-05-13 14:22:47 -0300

    feat: add imgPath fieln in schema prisma

---
+++ prisma/migrations/20240513172212_create_img_path_field/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "dentists" ADD COLUMN     "img_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "doctors" ADD COLUMN     "img_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "health_agents" ADD COLUMN     "img_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "health_units" ADD COLUMN     "img_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "nurses" ADD COLUMN     "img_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "nursing_technicians" ADD COLUMN     "img_path" TEXT;
