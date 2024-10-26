6ccc73291ece3c8e789fc78ca9d01fc78dc24764
GRDev <gierremartins@gmail.com>
2024-10-26 13:08:39 -0300

    remover migrations e ajustar .env

--- prisma/migrations/20240515222618_user_permissions/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'STAFF', 'ADMIN');
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'USER';
--- prisma/migrations/20240515222912_fix_permissions/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `role` column on the `users` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "role",
-ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
